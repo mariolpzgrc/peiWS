145dc37add997c4197321333d475fcd0bd74ec61
Mario López García <31970061+mariolpzgrc@users.noreply.github.com>
2021-02-19 18:14:18 -0600

    Update models.py
    Modificacion de  lo modelos para los servicios web

--- pei/models.py
+++ pei/models.py
@@ -11,7 +11,7 @@ class Usuario (models.Model):
 	celular = models.CharField(max_length=10)
 	correo = models.EmailField(max_length=50)
 	idFacultad = models.ForeignKey(Facultad, null=True, blank=True)
-	idPei = models.ForeignKey(PEI, null=True, blank=True)
+	idPei = models.ForeignKey(Pei, null=True, blank=True)
 	contrasena = models.CharField(max_length=50)
 	fechaCreacion = models.DateTimeField(auto_now_add=True)
 	fechaActualizacion = models.DateTimeField(auto_now=True)
@@ -19,7 +19,21 @@ class Usuario (models.Model):
 	def __str__(self):
 		return Usuario
 
-class Facultad(models.Model):
+class Pei(models.Model):
+	numeroPei = models.AutoField(primary_key=True)
+	nombrePei = models.TextField(max_length=100)
+	descripcionPei = models.TextField(max_length=300)
+	fechaIncicio = models.DateTimeField()
+	fechaTermino = models.DateTimeField()
+	numeroParticipantesPei = models.IntegerField(max_length=3)
+	fechaCreacion = models.DateTimeField(auto_now_add=True)
+	fechaActualizacion = models.DateTimeField(auto_now=True)
 
-class PEI(models.Model):
+class Participante(models,Model):
+	numeroPersonal = models.CharField(primary_key=True, max_length=10)
+	apellidoPaterno = models.TextField(max_length=50)
+	apellidoMaterno = models.TextField(max_length=50)
+	nombre = models.TextField(max_length=50)
+	idAreaAcademica = models.ForeignKey(AreaAcademica, null=False, blank=False)
+	idEntidad = models.ForeignKey(Entidad, null=False, blank=False)
 	
